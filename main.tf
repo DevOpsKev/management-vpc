6c847515de0c732847682f1975d2dfe12718b140
Kevin Ryan <36336550+DevOpsKev@users.noreply.github.com>
2020-05-31 18:50:34 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "devon"
+    organization = "devonnl-elsevier"
 
     workspaces {
       name = "learn-terraform-circleci"
